82cf8f65c77a5e950b509e386fd66069d3f59af4
Geovane Junior <geovane.gjunior@gmail.com>
2021-10-19 08:11:24 -0300

    Java Web - Desenvolvimento até aula 4

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pós Graduação - módulo Java Web
---
+++ src/main/java/br/com/geovanejunior/espjava/crudcidades/visao/Cidade.java
@@ -0,0 +1,16 @@
+package br.com.geovanejunior.espjava.crudcidades.visao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public final class Cidade {
+
+    private final String nome;
+    private final String estado;
+
+
+}
