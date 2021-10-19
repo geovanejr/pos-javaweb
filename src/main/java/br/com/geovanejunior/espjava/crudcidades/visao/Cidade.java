511d43be55120ed9ee99293068619bf0be191dea
Geovane Junior <geovane.gjunior@gmail.com>
2021-10-19 09:28:12 -0300

    Aula 05 - Incluindo Bean Validation

--- src/main/java/br/com/geovanejunior/espjava/crudcidades/visao/Cidade.java
+++ src/main/java/br/com/geovanejunior/espjava/crudcidades/visao/Cidade.java
@@ -4,12 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @Getter
 @Setter
 public final class Cidade {
 
+    @NotBlank(message = "{app.cidade.blank}")
+    @Size(min=5, max=60, message = "{app.cidade.size}")
     private final String nome;
+
+    @NotBlank(message = "app.estado.blank")
+    @Size(min=2, max=2, message = "{app.estado.size}")
     private final String estado;
 
 
