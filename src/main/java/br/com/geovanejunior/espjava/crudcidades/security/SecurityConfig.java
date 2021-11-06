42912ffa16e7abc07f66efeb939184034460cf51
Geovane Junior <geovane.gjunior@gmail.com>
2021-11-06 09:40:08 -0300

    Aula 07 - Autorizacao

--- src/main/java/br/com/geovanejunior/espjava/crudcidades/security/SecurityConfig.java
+++ src/main/java/br/com/geovanejunior/espjava/crudcidades/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.geovanejunior.espjava.crudcidades.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +24,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("ana")
                 .password(cifrador().encode("test123"))
                 .authorities("admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").hasAnyAuthority("listar", "admin")
+                .antMatchers("/criar").hasAnyAuthority("admin")
+                .antMatchers("/excluir").hasAnyAuthority("admin")
+                .antMatchers("/preparaAlterar").hasAnyAuthority("admin")
+                .antMatchers("/alterar").hasAnyAuthority("admin")
+                .anyRequest().denyAll()
+                .and()
+                .formLogin().permitAll();
     }
 
     @Bean
